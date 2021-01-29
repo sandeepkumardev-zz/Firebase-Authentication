21498642aba7cfb72d7cd524dd412748745ca126
Sandeep Kumar <sandeepypb@gmail.com>
2021-01-29 12:03:40 +0530

    basic router setup

---
+++ src/auth/SignInPage.js
@@ -0,0 +1,25 @@
+import { Grid, Paper } from "@material-ui/core";
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
+
+function SignInPage() {
+  const classes = useStyles();
+
+  return (
+    <form className={classes.root} noValidate autoComplete="off">
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    </form>
+  );
+}
+
+export default SignInPage;
---
+++ src/components/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <div>Bookmark Application</div>;
+}
+
+export default Home;
